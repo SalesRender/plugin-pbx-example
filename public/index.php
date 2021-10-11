43207c2727e120d55308303be2980dbc59faf8c0
disami115 <disami115@yandex.ru>
2021-10-11 13:52:24 +0300

    Fix index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,5 @@ use Leadvertex\Plugin\Core\PBX\Factories\WebAppFactory;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $factory = new WebAppFactory();
-$factory->addUploadAction();
 $application = $factory->build();
 $application->run();
